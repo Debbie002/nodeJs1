2b2a29846a31c2ee7bde0a6e902292314d24dd83
Debbie002 <deborahpeter011@gmail.com>
2024-05-15 23:22:23 +0100

    hh

---
+++ server.js
@@ -0,0 +1,30 @@
+const https = require("https");
+const { title } = require("process");
+const db = [
+    {
+        title: "laugh",
+        comedian: "me",
+        year: 2024,
+        id: 1,
+    },
+    {
+        title: "laugh",
+        comedian: "me",
+        year: 2024,
+        id: 2,
+    },
+    {
+        title: "laugh",
+        comedian: "me",
+        year: 2024,
+        id: 3,
+    }
+]
+const server = https.createServer(()=>{
+
+})
+
+
+server.listen(3000, (req, res) =>{
+    console.log("am running");
+})
